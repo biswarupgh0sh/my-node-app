216f2ad78b1dcdc0f4cf44cb233ae18f5d87152c
Biswarup <biswarupghosh520@gmail.com>
2025-02-11 00:12:06 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res)=>{
+    res.send("hello");
+})
 
 app.listen(PORT, () => {
     console.log(`The server is listening on port: ${PORT}`)
